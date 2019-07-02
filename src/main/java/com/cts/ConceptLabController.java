f46e08dfd5e78c95469602df2aa04b70a82bb043
NithyaRajamanickam <40426789+NithyaRajamanickam@users.noreply.github.com>
2019-07-02 17:00:45 +0530

    commit

---
+++ src/main/java/com/cts/ConceptLabController.java
@@ -0,0 +1,37 @@
+package com.cts;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ConceptLabController {
+
+	@RequestMapping("/requirement")
+	public ModelAndView requirement(){
+		//System.out.println(String.format("The value of my integer is %d", "Hello World"));  // Noncompliant; an 'int' is expected rather than a String)
+		return new ModelAndView("requirementPage");
+	}	
+	
+	@RequestMapping("/designAndCode")
+	public ModelAndView designAndCode(){
+		return new ModelAndView("designAndCodePage");
+	}
+	
+	@RequestMapping("/testing")
+	public ModelAndView testing(){
+		return new ModelAndView("testingPage");
+	}
+	
+	@RequestMapping("/endToEnd")
+	public ModelAndView endToEnd(){
+		return new ModelAndView("endtoEndPage");
+	}
+	
+	@RequestMapping("/projectManagement")
+	public ModelAndView projectManagement(){
+		return new ModelAndView("projectManagementPage");
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/jsp/designAndCodePage.jsp
@@ -0,0 +1,34 @@
+<html>
+<body bgcolor="#ffe6e6">
+<div style="width:100%;">
+	<div style="width:100%;height:10%;text-align:center;">
+		<h2>Design, Develop and Deploy workflow</h2>
+	</div>
+	<div style="width:100%;text-align:center;">
+		
+		<table border="1">
+			<tr>
+				<th>Prerequisite for Installation</th>
+				<th>Mail</th>
+			</tr>
+			<tr>
+				<td>
+					<ul>
+						<li>RAD - For coding/developmet (Java)</li>
+						<li>Desktop client for following tools:</li>											
+					</ul>
+				</td>
+				<td>
+					Please send a mail to CLMadmin@cognizant.com along with the following details:
+					<ul>
+						<li>Duration (Maximum 20 days) for which you need access to the application</li>
+						<li>Git Repository where the code will reside.</li>
+					</ul>
+				</td>
+			</tr>
+		</table>
+	</div>
+	
+</div>
+</body>
+</html>
