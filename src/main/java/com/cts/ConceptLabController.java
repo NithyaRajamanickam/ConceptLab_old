d213c79f6654b48417025842cfd8a468853df474
123593 <sathishkumar.gururajan@cognizant.com>
2019-07-03 16:08:42 +0530

    demo vk

--- src/main/java/com/cts/ConceptLabController.java
+++ src/main/java/com/cts/ConceptLabController.java
@@ -9,7 +9,8 @@ import org.springframework.web.servlet.ModelAndView;
 public class ConceptLabController {
 
 	@RequestMapping("/requirement")
-	public ModelAndView requirement(){		
+	public ModelAndView requirement(){	
+		
 		return new ModelAndView("requirementPage");
 	}	
 	
