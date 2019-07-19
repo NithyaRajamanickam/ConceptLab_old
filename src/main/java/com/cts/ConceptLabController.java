b48d8df2679d2c5d99f5e6b3d032d32775ea3f38
NithyaRajamanickam <40426789+NithyaRajamanickam@users.noreply.github.com>
2019-07-19 14:53:27 +0530

    commit

--- src/main/java/com/cts/ConceptLabController.java
+++ src/main/java/com/cts/ConceptLabController.java
@@ -26,7 +26,7 @@ public class ConceptLabController {
 	
 	@RequestMapping("/designAndCode")
 	public ModelAndView designAndCode(){	
-		//System.out.println(String.format("The value of my integer is %d", "Hello World"));  // Noncompliant; an 'int' is expected rather than a String)		
+		System.out.println(String.format("The value of my integer is %d", "Hello World"));  // Noncompliant; an 'int' is expected rather than a String)		
 		return new ModelAndView("designAndCodePage");
 	}
 	
