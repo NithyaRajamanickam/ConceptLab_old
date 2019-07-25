535788c064a28235ff6b3fd33451fe0874649538
NithyaRajamanickam <40426789+NithyaRajamanickam@users.noreply.github.com>
2019-07-25 14:56:11 +0530

    test

--- src/main/java/com/cts/ConceptLabController.java
+++ src/main/java/com/cts/ConceptLabController.java
@@ -32,7 +32,15 @@ public class ConceptLabController {
 	
 	@RequestMapping("/projectManagement")
 	public ModelAndView projectManagement(){
-		
+		int param = 1;
+		if (param == 1)
+		 System.out.println("foo");
+		else if (param == 2)
+		 System.out.println("foo");
+		else if (param == 1) 
+		{// Noncompliant
+		  System.out.println("foo");
+		}
 		return new ModelAndView("projectManagementPage");
 	}
 	
