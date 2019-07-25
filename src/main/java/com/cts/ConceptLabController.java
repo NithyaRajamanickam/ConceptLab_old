3dc5d92bea922617c5bbd4cf12ae63a5a7908506
NithyaRajamanickam <40426789+NithyaRajamanickam@users.noreply.github.com>
2019-07-25 15:48:49 +0530

    test

--- src/main/java/com/cts/ConceptLabController.java
+++ src/main/java/com/cts/ConceptLabController.java
@@ -9,7 +9,8 @@ import org.springframework.web.servlet.ModelAndView;
 public class ConceptLabController {
 
 	@RequestMapping("/requirement")
-	public ModelAndView requirement(){		
+	public ModelAndView requirement(){	
+		int b = a > 12 ? 4 : 4;
 		return new ModelAndView("requirementPage"); 
 	}	
 	
