30621e80d0ed5ef5e4b3e9f46c81806bab23c753
NithyaRajamanickam <40426789+NithyaRajamanickam@users.noreply.github.com>
2019-07-26 11:43:44 +0530

    test

--- src/main/java/com/cts/ConceptLabController.java
+++ src/main/java/com/cts/ConceptLabController.java
@@ -25,7 +25,7 @@ public class ConceptLabController {
 	@RequestMapping("/designAndCode")
 	public ModelAndView designAndCodePage()
 	{
-		
+		  
 		return new ModelAndView("designAndCodePage"); 
 	}
 	
