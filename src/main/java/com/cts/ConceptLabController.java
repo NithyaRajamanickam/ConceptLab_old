bb7171303265006f9f34c26a8d8f169dae1418b7
NithyaRajamanickam <40426789+NithyaRajamanickam@users.noreply.github.com>
2019-07-26 10:51:58 +0530

    updated file

--- src/main/java/com/cts/ConceptLabController.java
+++ src/main/java/com/cts/ConceptLabController.java
@@ -10,7 +10,15 @@ public class ConceptLabController {
 
 	@RequestMapping("/requirement")
 	public ModelAndView requirement(){	
-		System.out.println(String.format("The value of my integer is %d", "Hello World"));  // Noncompliant; an 'int' is expected rather than a String)
+		int param = 1;
+		if (param == 1)
+		 System.out.println("foo");
+		else if (param == 2)
+		 System.out.println("foo");
+		else if (param == 1) 
+		{// Noncompliant
+		  System.out.println("foo");
+		}
 		return new ModelAndView("requirementPage"); 
 	}	
 	
