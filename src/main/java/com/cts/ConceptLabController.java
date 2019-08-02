cedeaa37e8333cf902246476d065b637da06ae0c
NithyaRajamanickam <40426789+NithyaRajamanickam@users.noreply.github.com>
2019-08-02 18:47:13 +0530

    Update ConceptLabController.java

--- src/main/java/com/cts/ConceptLabController.java
+++ src/main/java/com/cts/ConceptLabController.java
@@ -16,15 +16,7 @@ public class ConceptLabController {
 	@RequestMapping("/designAndCode")
 	public ModelAndView designAndCodePage()
 	{	  
-		int param = 1;
-		if (param == 1)
-		 System.out.println("foo");
-		else if (param == 2)
-		 System.out.println("foo");
-		else if (param == 1) 
-		{// Noncompliant
-		  System.out.println("foo");
-		}
+		
 		return new ModelAndView("designAndCodePage"); 
 	}
 	
