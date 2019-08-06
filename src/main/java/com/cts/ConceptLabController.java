e830c52ade2f2d48d245b85bdc4e78b580f46e97
NithyaRajamanickam <40426789+NithyaRajamanickam@users.noreply.github.com>
2019-08-06 11:41:57 +0530

    Update ConceptLabController.java

--- src/main/java/com/cts/ConceptLabController.java
+++ src/main/java/com/cts/ConceptLabController.java
@@ -17,16 +17,7 @@ public class ConceptLabController {
 	@RequestMapping("/designAndCode")
 	public ModelAndView designAndCodePage()
 	{			
-		int param = 1;
-		if (param == 1)
-		 System.out.println("foo");
-		else if (param == 2)
-		 System.out.println("foo");
-		else if (param == 1) 
-		{// Noncompliant
-		  System.out.println("foo");
-		}	
-		
+		System.out.println(String.format("The value of my integer is %d", "Hello World"));  // Noncompliant; an 'int' is expected rather than a String) 
 		return new ModelAndView("designAndCodePage"); 
 	}
 	
