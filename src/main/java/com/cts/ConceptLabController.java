962821e886b60d7081d7a985683b2e79ea1c8d3d
NithyaRajamanickam <40426789+NithyaRajamanickam@users.noreply.github.com>
2019-08-06 12:39:24 +0530

    test

--- src/main/java/com/cts/ConceptLabController.java
+++ src/main/java/com/cts/ConceptLabController.java
@@ -35,7 +35,7 @@ public class ConceptLabController {
 	
 	@RequestMapping("/endToEnd")
 	public ModelAndView endToEnd(){
-		System.out.println(String.format("The value of my integer is %d", "Hello World"));  // Noncompliant; an 'int' is expected rather than a String)
+		
 		return new ModelAndView("endtoEndPage");
 	}
 	
