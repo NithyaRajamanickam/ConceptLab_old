9ffbb93b1d9562cf7e8f394eb69f53b87cf641ac
NithyaRajamanickam <40426789+NithyaRajamanickam@users.noreply.github.com>
2019-08-06 12:04:41 +0530

    removed

--- src/main/java/com/cts/ConceptLabController.java
+++ src/main/java/com/cts/ConceptLabController.java
@@ -1,7 +1,7 @@
 package com.cts;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
