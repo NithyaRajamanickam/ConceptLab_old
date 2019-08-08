28ef9d9418867eebc34d31bcc18d0162b617d311
sv <sv@gmail.com>
2019-08-08 14:58:39 +0530

    test

--- src/main/java/com/cts/ConceptLabController.java
+++ src/main/java/com/cts/ConceptLabController.java
@@ -21,7 +21,6 @@ public class ConceptLabController {
 	
 	@RequestMapping("/testing")
 	public ModelAndView testing(){	
-		System.out.println(String.format("The value of my integer is %f", "Hello Worlddd"));  // Noncompliant; an 'int' is expected rather than a String)
 		return new ModelAndView("testingPage");
 	}
 	
