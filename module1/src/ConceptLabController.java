9fb28d12aac097cb3fe297cb54a7b9bebd412d35
antony <mark.antonyi@cognizant.com>
2019-08-14 15:10:26 +0530

    test

---
+++ module1/src/ConceptLabController.java
@@ -0,0 +1,41 @@
+package com.cts;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ConceptLabController {
+
+	@RequestMapping("/requirement")
+	public ModelAndView requirement(){ 
+		 
+		return new ModelAndView("requirementPage"); 
+	}	
+	
+	@RequestMapping("/designAndCode")
+	public ModelAndView designAndCodePage()
+	{
+		
+		return new ModelAndView("designAndCodePage");
+	}
+	
+	@RequestMapping("/testing")
+	public ModelAndView testing(){	
+		return new ModelAndView("testingPage");
+	}
+	
+	@RequestMapping("/endToEnd")
+	public ModelAndView endToEnd(){	
+		
+		return new ModelAndView("endtoEndPage");
+	}
+	
+	@RequestMapping("/projectManagement")
+	public ModelAndView projectManagement(){	
+		
+		return new ModelAndView("projectManagementPage");
+	}
+	
+}
